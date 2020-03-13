d0df1e98513dce676c39181a87b6370b25a466d0
Kevin <kevinbluth5@gmail.com>
2020-03-13 13:36:41 -0600

    prop it up

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ function App() {
   return (
     <div className="App">
       <Person firstName="Kevin" lastName="Bluth" age={23} hairColor="Brown" />
+      <Person firstName="Doug" lastName="Red" age={38} hairColor="Brown" />
+      <Person firstName="Lisa" lastName="Orange" age={16} hairColor="Brown" />
+      <Person firstName="Kate" lastName="Yellow" age={71} hairColor="Brown" />
     </div>
   );
 }
--- src/components/Person.js
+++ src/components/Person.js
@@ -3,10 +3,10 @@ import React, { Component } from "react";
 class Person extends Component {
     render() {
         return(
-            <div>
-                <h1>Name: {this.props.firstName} {this.props.lastName}</h1>
-                <h4>Age: {this.props.age}</h4>
-                <h4>Hair Color: {this.props.hairColor}</h4>
+            <div style={{width:"250px", marginLeft:"40%", marginTop:"10px", padding:"0px 15px 0px 15px", border:"solid black 2px",}}>
+                <h2 style={{textAlign:"left"}}>Name: {this.props.firstName} {this.props.lastName}</h2>
+                <h5 style={{textAlign:"left"}}>Age: {this.props.age}</h5>
+                <h5 style={{textAlign:"left"}}>Hair Color: {this.props.hairColor}</h5>
             </div>
         );
     }
